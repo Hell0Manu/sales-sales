9698b7268956dd0d8e7c90ec3a1d144fc3ba129f
Hell0Manu <emanuelle.genesio@estudante.iftm.edu.br>
2024-04-15 13:01:23 -0300

    tudo funcionando, apenas faltando alguns ajustes

--- sales-main/script.js
+++ sales-main/script.js
@@ -1,8 +1,10 @@
 const dropdown =  document.querySelector('.dropdown');
 const menu = document.querySelector('.dropdown-menu');
+const arrow = document.querySelector('.arrow-space')
 
 dropdown.onclick = () => {
-    menu.classList.toggle('ativo')
+    menu.classList.toggle('ativo');
+    arrow.classList.toggle('ativo');
 }
 
 function menuShow() {
@@ -19,3 +21,22 @@ function menuShow() {
         iconMobile.style.transform = 'rotate(43deg)';
     }
 }
+
+
+function toggleMobileDropdown() {
+    let menuMobile = document.querySelector('.dropdown-menu-mobile');
+    let iconMobile = document.querySelector('.icon button');
+
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        iconMobile.style.transform = 'rotate(0deg)';
+
+
+    }
+    else {
+        menuMobile.classList.add('open');
+        iconMobile.style.transform = 'rotate(180deg)';
+    }
+}
+
+
