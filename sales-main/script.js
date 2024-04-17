dd8a27324e0e30ede6464145d23ff2056598290d
Hell0Manu <emanuelle.genesio@estudante.iftm.edu.br>
2024-04-17 14:50:21 -0300

    overflow-y, height precisa ser auto

--- sales-main/script.js
+++ sales-main/script.js
@@ -1,12 +1,13 @@
 const dropdown =  document.querySelector('.dropdown');
 const menu = document.querySelector('.dropdown-menu');
-const arrow = document.querySelector('.arrow-space')
+const arrow = document.querySelector('.box-arrow-space')
 
 dropdown.onclick = () => {
     menu.classList.toggle('ativo');
     arrow.classList.toggle('ativo');
 }
 
+/* Abre o mobile-menu e rotacio o arraw correspondente */
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     let iconMobile = document.querySelector('.icon-arraw-mobile')
@@ -22,17 +23,14 @@ function menuShow() {
     }
 }
 
-
+/* Abre a sessão "Produtos de Vendas" e rotaciona o arrow dessa sessão */ 
 function toggleMobileDropdown() {
     let menuMobile = document.querySelector('.dropdown-menu-mobile');
-    let iconMobile = document.querySelector('.icon button');
     let iconstyle = document.querySelector('.icon-icon-arraw');
 
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         iconstyle.style.transform = 'rotate(0deg)';
-
-
     }
     else {
         menuMobile.classList.add('open');
